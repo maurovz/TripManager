5d9ee2c80c1504e1b49984147eb6d98fe9291a57
Mauricio Vazquez <maurovz@gmail.com>
2020-04-24 13:14:35 +0200

    Initial Commit

---
+++ TripManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripManager
+//
+//  Created by Mauricio Vazquez on 4/24/20.
+//  Copyright © 2020 CoderCamp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
