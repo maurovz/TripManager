e8a3c252058c407d9dfff5ad198d25dd24a1f49b
Mauricio Vazquez <maurovz@gmail.com>
2020-05-05 16:48:06 +0200

    added mapview preview (#9)

--- TripManager/Views/MapView.swift
+++ TripManager/Views/MapView.swift
@@ -42,10 +42,10 @@ struct MapView: UIViewRepresentable {
   }
 }
 
-//struct InitialMapView_Previews: PreviewProvider {
-//  static var previews: some View {
-//    let trip = Trip(status: "", origin: Origin(address: "John St", point: Point(_latitude: 300.00, _longitude: 300.00)), stops: [Stop(point: Point(_latitude: 300, _longitude: 300), id: 3)], destination: Destination(address: "Peer St", point: Point(_latitude: 300, _longitude: 300)), endTime: "300", startTime: "300", description: "Hellooo Hellooo Hellooo  ", driverName: "Ben", route: "Laawwwl")
-//    let tripViewModel = TripViewModel(trip: trip)
-//    return MapView(tripViewModel: tripViewModel)
-//  }
-//}
+struct InitialMapView_Previews: PreviewProvider {
+  static var previews: some View {
+    let trip = Trip(status: "", origin: Origin(address: "John St", point: Point(_latitude: 300.00, _longitude: 300.00)), stops: [StopInfo(point: Point(_latitude: 300, _longitude: 300), id: 3)], destination: Destination(address: "Peer St", point: Point(_latitude: 300, _longitude: 300)), endTime: "300", startTime: "300", description: "Route Description ", driverName: "Ben", route: "PTY6y7d68")
+    let tripViewModel = TripViewModel(trip: trip)
+    return MapView(tripViewModel: tripViewModel)
+  }
+}
